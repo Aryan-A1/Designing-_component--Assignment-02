c50a9fe5e733bbccaab685676d3b969d0400ff7f
Aryan-A1 <aryankumarsingh9370@gmail.com>
2025-03-12 10:48:24 +0530

    Postcard

--- src/components/productcard.jsx
+++ src/components/productcard.jsx
@@ -1,1 +1,33 @@
-// write product card here
+import "../components/productcard.css";
+import Button from "./button";
+
+export default function ProductCard() {
+  const products = [
+    {
+      product_image: "https://pbs.twimg.com/media/GgqTCL4XkAAuW7N.jpg",
+      product_name: "NVIDIA GeForce RTX 5090",
+      price: "3000$",
+    },
+    {
+      product_image: "https://i.ytimg.com/vi/fj245xMr-BM/sddefault.jpg",
+      product_name: "NVIDIA GeForce RTX 4090",
+      price: "2500$",
+    },
+  ];
+
+  return (
+    <div className="product-container">
+      {products.map((product, index) => (
+        <div key={index} className="product-card">
+          <img src={product.product_image} alt="Product-image" className="product-img" />
+          <h3>{product.product_name}</h3>
+          <h2>
+            <b>Price: </b>
+            {product.price}
+          </h2>
+          <Button />
+        </div>
+      ))}
+    </div>
+  );
+}
